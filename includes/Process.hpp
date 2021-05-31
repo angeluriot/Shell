fee0f496b4526e9fa40190fea989fd92d5edc13e
D1MENSI0N <angel.uriot@gmail.com>
2021-05-31 11:38:56 +0200

    Pocess upadte

--- includes/Process.hpp
+++ includes/Process.hpp
@@ -1,9 +1,17 @@
 #ifndef PROCESS_HPP
 #define PROCESS_HPP
+#include <unistd.h>
+#include <vector>
+#include <string>
 
 class Process
 {
-
+	struct process *next;			/* next process in pipeline */
+	std::vector<std::string> argv;	/* for exec */
+	pid_t pid;						/* process ID */
+	char completed;					/* true if process has completed */
+	char stopped;					/* true if process has stopped */
+	int status;						/* reported status value */
 };
 
 #endif
