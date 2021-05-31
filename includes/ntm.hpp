db29b9bb690298b63d1484e35f9fe3ac3b876e41
D1MENSI0N <angel.uriot@gmail.com>
2021-05-31 10:42:06 +0200

    Init

---
+++ includes/ntm.hpp
@@ -0,0 +1,6 @@
+#ifndef NTM_HPP
+#define NTM_HPP
+
+constexpr int ntm = 18;
+
+#endif
---
+++ sources/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	int i = 57875;
+	std::cout << "fgiuezrogbieusrgbuolzsero" << std::endl;
+	std::cin.get();
+	return 0;
+}
